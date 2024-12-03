7c740fca15cded09a821ddaae0fe8efcb870f7b9
Tania Evora Vargas Martinez <72502920+Tania-Vargas@users.noreply.github.com>
2024-12-03 06:46:42 +0000

    refactorizado

--- d3.py
+++ d3.py
@@ -11,34 +11,29 @@ lines = ""
 with open(file, 'r') as f:
   lines = f.readlines()
 # Parte 1
-multiplicacion = []
-for l in lines:
-  multiplicacion += re.findall(r'mul\(\d{1,3},\d{1,3}\)', l)
+def find_match(regex, lines):
+  match = []
+  for l in lines:
+    match += re.findall(regex, l)
+  return match
 
+def mul(mul):
+  d = re.findall(r'\d{1,3}', mul)
+  return int(d[0]) * int(d[1]) 
+
+multiplicacion = find_match(r'mul\(\d{1,3},\d{1,3}\)', lines)
 r1 = 0
 for m in multiplicacion:
-  d = re.findall(r'\d{1,3}', m)
-  r1 += int(d[0]) * int(d[1]) 
-
+  r1 += mul(m)
 print(f"Parte 1: {r1}")
 
 # Parte 2
-new_mult = []
-for l2 in lines:
-  new_mult += re.findall(r'don\'t\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)', l2)
-
+new_mult = find_match(r'don\'t\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)', lines)
 r2 = 0
 operate = True
 for m2 in new_mult:
-  if m2 == 'don\'t()': 
-    operate = False
-  elif m2 == 'do()':
-    operate = True
+  if m2 == 'don\'t()': operate = False
+  elif m2 == 'do()': operate = True
   else:
-    if operate == True:
-      d = re.findall(r'\d{1,3}', m2)
-      d1 = int(d[0])
-      d2 = int(d[1])
-      r2 += d1 * d2
-
+    if operate == True: r2 += mul(m2)
 print(f"Parte 2: {r2}")
